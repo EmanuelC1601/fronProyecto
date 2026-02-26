23f552f5f792f2464a239a93cd25e815a9733b85
EmanuelC1601 <emanuelcruz123c@gmail.com>
2026-02-26 11:21:47 -0600

    fron modifcado
    fron actualizado

--- src/app/services/mensaje.service.ts
+++ src/app/services/mensaje.service.ts
@@ -25,7 +25,7 @@ export interface PaginatedResponse {
   providedIn: 'root'
 })
 export class MensajeService {
-  private apiUrl = 'http://localhost:3000/api/mensajes'; // Ajusta según tu backend
+  private apiUrl = 'https://backend-bhit.onrender.com/api'; // Ajusta según tu backend
 
   constructor(private http: HttpClient) {}
 
