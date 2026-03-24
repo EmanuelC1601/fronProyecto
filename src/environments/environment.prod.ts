4261fe04f84d4fa3031a4767e26142c4432de0b2
EmanuelC1601 <emanuelcruz123c@gmail.com>
2026-03-24 12:15:05 -0600

    ajuste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://backend-bhit.onrender.com/api',
-  recaptchaSiteKey: '6LfgNpUsAAAAAIxWpy9erP1p4bq9Z7hy9SEUA4lV'
+  recaptchaSiteKey: '6Lehh5YsAAAAALC4wBwdatLpSZNVgjq_BTtHyTg6'
 };
