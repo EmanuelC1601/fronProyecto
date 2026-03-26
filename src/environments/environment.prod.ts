f9c7a41837acf0cc6cb3420cf9cb94f47dfddb78
EmanuelC1601 <emanuelcruz123c@gmail.com>
2026-03-25 19:12:20 -0600

    correcion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
+// environment.production.ts
 export const environment = {
   production: true,
   apiUrl: 'https://backend-bhit.onrender.com/api',
-  recaptchaSiteKey: '6LfDUpgsAAAAAA3h9YkW9l1i1UwDDTOSiKpn00tu'
+  recaptchaSiteKey: '6LfDUpgsAAAAACBItj77fl1JzDYtEiaqTEj2gk9S'  // ✅ Site Key
 };
